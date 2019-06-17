a611cfe3978c350615743a614b06896fc9c80eb2
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-06-17 22:53:55 +0200

    Initial file structure

---
+++ modules/server.js
@@ -0,0 +1,14 @@
+function start() {
+    function onRequest(request, response) {
+      console.log("Odebrano zapytanie.");
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write("Pierwsze koty za płoty");
+      response.end();
+    }
+  
+    http.createServer(onRequest).listen(9000);
+  
+    console.log("Uruchomiono serwer!".green);
+  }
+  
+  exports.start = start;
