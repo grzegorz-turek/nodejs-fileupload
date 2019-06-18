c0ceb7b130ddd68d6bcb488b6404e36e255643a1
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-06-18 21:35:55 +0200

    Final v.2

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
 var formidable = require('formidable');
+//var path = require('path');
 
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
-    form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
+    form.parse(request, function(err, fields, files) {
+        if (err) throw err
+        fs.renameSync(files.upload.path, "images/test.png");
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
         response.write("<img src='/show' />");
@@ -16,14 +18,10 @@ exports.upload = function(request, response) {
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsługę żądania welcome.");
     fs.readFile('templates/start.html', 'utf-8', function(err, html){
-        if (err) {
-            response.write('<h1>Can\'t read start.html</h1>'); // DEL
-            response.end();
-        } else {
-            response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-            response.write(html);
-            response.end();
-        }
+        if (err) throw err;
+        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write(html);
+        response.end();
     });
 }
 
@@ -34,7 +32,8 @@ exports.error = function(request, response) {
 }
 
 exports.show = function(request, response) {
-    fs.readFile("test.png", "binary", function(error, file) { // ZMIANA ERROR NA ERR???
+    fs.readFile("images/test.png", "binary", function(err, file) { // ZMIANA ERROR NA ERR???
+        if(err) throw err;
         response.writeHead(200, {"Content-Type": "image/png"});
         response.write(file, "binary");
         response.end();
