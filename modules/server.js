0a274368e9ddc555d7583b75cc1fc35fd45c8cb3
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-06-18 02:10:32 +0200

    Final

--- modules/server.js
+++ modules/server.js
@@ -1,14 +1,54 @@
+var http = require('http');
+var colors = require('colors');
+var handlers = require('./handlers');
+
 function start() {
     function onRequest(request, response) {
-      console.log("Odebrano zapytanie.");
-      response.writeHead(200, {"Content-Type": "text/plain"});
-      response.write("Pierwsze koty za płoty");
-      response.end();
+        console.log("Odebrano zapytanie.".green);
+        console.log("Zapytanie " + request.url + " odebrane.");
+        response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+        switch (request.url) {
+            case '/':
+            case '/start':
+                handlers.welcome(request, response);
+                break;
+            case '/upload':
+                handlers.upload(request, response);
+                break;
+            case '/show':
+                handlers.show(request, response);
+                break;
+            default:
+                handlers.error(request, response);
+        }
     }
-  
     http.createServer(onRequest).listen(9000);
-  
     console.log("Uruchomiono serwer!".green);
-  }
-  
-  exports.start = start;
+}
+
+/*
+// ver 2.0
+function start() {
+    var onRequest = http.createServer();
+    onRequest.on('request', function (request, response) {
+        console.log("Odebrano zapytanie.".green);
+        console.log("Zapytanie " + request.url + " odebrane.");
+        response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+        switch (request.url) {
+            case '/':
+            case '/start':
+                handlers.welcome(request, response);
+                break;
+            case '/upload':
+                handlers.upload(request, response);
+                break;
+            default:
+                handlers.error(request, response);
+        }
+    });
+    onRequest.listen(9000);
+    console.log("Uruchomiono serwer!".green);
+}
+*/
+
+exports.serverStart = start;
